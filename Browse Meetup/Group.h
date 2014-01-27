e478d9365f583a29b4ca375ad830cd92a6202bfb
dngo <david.ngo@slingmedia.com>
2014-01-27 16:09:41 -0500

    rendering channels

---
+++ Browse Meetup/Group.h
@@ -0,0 +1,14 @@
+//
+//  Group.h
+//  BrowseMeetup
+//
+//  Created by Simon on 31/8/13.
+//  Copyright (c) 2013 TAMIM Ziad. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Group : NSObject
+@property (strong, nonatomic) NSString *channelId;
+@property (strong, nonatomic) NSString *channelTitle;
+@end
